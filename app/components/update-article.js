67d3e980be2fa0decacb51d1c47c1d1ba98c6d33
Dallas Slaughter and Kaya Winn <kaya@>
2017-04-25 16:33:10 -0700

    Add everything

---
+++ app/components/update-article.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateArticleForm: false,
+  actions: {
+    updateArticleForm() {
+      this.set('updateArticleForm', true)
+    },
+    update(article) {
+      var params = {
+        title: this.get('title'),
+        body: this.get('body'),
+        date: this.get('date'),
+        imageURL: this.get('imageURL'),
+        author: this.get('author'),
+        featured: this.get('featured')
+      }
+      this.set('updateArticleForm', false)
+      this.sendAction('update', article, params)
+    }
+  }
+});
---
+++ config/environment.js
@@ -0,0 +1,54 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'bustle-clone',
+    environment: environment,
+    baseURL: '/',
+    locationType: 'auto',
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    },
+
+    firebase: {
+      apiKey: "AIzaSyBaB0kiwxv2kaokD_DSZpYxCycsRxx6-zI",
+      authDomain: "bustle-clone-42dd6.firebaseapp.com",
+      databaseURL: "https://bustle-clone-42dd6.firebaseio.com",
+       storageBucket: "bustle-clone-42dd6.appspot.com",
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.baseURL = '/';
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
