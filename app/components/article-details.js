9389dc9873dc61890a6bff2cee3805febb8dfd3b
Blake Womack and Kaya Winn <kaya@>
2017-04-26 16:05:07 -0700

    add actions on article-detail.js

--- app/components/article-details.js
+++ app/components/article-details.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+  delete(article){
+    if (confirm('Are you sure?')){
+      this.sendAction('destroyArticle', article);
+      }
+    }
+  }
 });
--- app/routes/article.js
+++ app/routes/article.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('article', params.article_id);
+  },
+
+  actions: {
+    destroyArticle(article){
+      article.destroyArticle();
+      this.transitionTo('index');
+    }
   }
 });
